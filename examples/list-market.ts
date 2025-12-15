13ea332bb73f6d13a4145d7a387cefe8df2348b4
PolyEmperion <polyemperion@gmail.com>
2025-12-15 11:24:39 +0700

    list-market.ts

---
+++ examples/list-market.ts
@@ -0,0 +1,13 @@
+import { PolyEmperionSDK } from "../src/client.js";
+
+async function main() {
+  const sdk = new PolyEmperionSDK({ debug: true });
+  const markets = await sdk.getMarkets();
+  console.log("Total markets:", markets.length);
+  console.log("First market:", markets[0]);
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
